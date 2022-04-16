23dca7261618436179bc19b3012754f64ba7688b
Shanjida Rahman Maria <shanjidamaria07@gmail.com>
2022-04-17 01:39:11 +0600

    banner part added

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,14 +1,15 @@
 import React from "react";
 import { Container, Nav, Navbar, Offcanvas } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import "./Header.css";
 
 const Header = () => {
   return (
     <div>
-      <Navbar className=" bg-stone-800" expand={false}>
+      <Navbar className=" text-slate-100" expand={false}>
         <Container>
           <Navbar.Brand href="#">
-            <span className="text-neutral-200">Emma Watson</span>
+            <span className=" text-2xl">Emma Watson</span>
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="offcanvasNavbar" />
           <Navbar.Offcanvas
@@ -18,16 +19,21 @@ const Header = () => {
           >
             <Offcanvas.Header closeButton>
               <Offcanvas.Title id="offcanvasNavbarLabel">
-                Emma Watson
+                <span className=" text-4xl font-semibold text-gray-800">
+                  Emma Watson
+                </span>
               </Offcanvas.Title>
             </Offcanvas.Header>
             <Offcanvas.Body>
               <Nav className="justify-content-end flex-grow-1 pe-3 ">
-                <Nav.Link classname=" text-neutral-900" as={Link} to="aboutme">
-                  About Me
+                <Nav.Link as={Link} to="home">
+                  <span className="link">HOME</span>
+                </Nav.Link>
+                <Nav.Link as={Link} to="aboutme">
+                  <span className="link">ABOUT ME</span>
                 </Nav.Link>
                 <Nav.Link as={Link} to="blogs">
-                  Blogs
+                  <span className="link">BLOGS</span>
                 </Nav.Link>
               </Nav>
             </Offcanvas.Body>
---
+++ src/Components/Pages/Home/Banner/Banner.js
@@ -0,0 +1,59 @@
+import React from "react";
+import { Carousel, Container } from "react-bootstrap";
+import "./Banner.css";
+import image2 from "../../../../images/HeaderImg/emma-watson-header.png";
+import image1 from "../../../../images/HeaderImg/header-1.png";
+
+const Banner = () => {
+  return (
+    <div className="head bg-black">
+      <Container fluid>
+        <Carousel fade>
+          <Carousel.Item>
+            <img className="d-block w-100" src={image1} alt="Second slide" />
+            <Carousel.Caption>
+              <div className=" text-xs lg:text-lg text-left md:mb-60">
+                <h3 className=" text-gray-400">
+                  {" "}
+                  <span className=" text-gray-300 md:text-6xl">
+                    Emma Watson
+                  </span>
+                  , <br /> A Living inspiration for women
+                </h3>
+                <p className="mt-3 w-3/4">
+                  Emma Watson is best known for playing the character of
+                  Hermione Granger, one of Harry Potter's best friends in the
+                  'Harry Potter' film franchise.When Watson was appointed as the
+                  UN Women Goodwill Ambassador, she became a vigorous and active
+                  voice of our generation, along with a player in modern
+                  feminism.
+                </p>
+              </div>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img className="d-block w-100" src={image2} alt="Second slide" />
+            <Carousel.Caption>
+              <div className=" text-xs lg:text-lg text-left md:mb-60">
+                <h3 className=" text-gray-400">
+                  {" "}
+                  <span className=" text-gray-300 md:text-6xl">
+                    Emma Watson
+                  </span>
+                  , <br />
+                  serves as positive influence, role model
+                </h3>
+                <p className="mt-3 w-3/4">
+                  From Hermione to Little Women: Emma Watson, a True Role Model
+                  for Millennials!
+                </p>
+              </div>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
+      </Container>
+    </div>
+  );
+};
+
+export default Banner;
