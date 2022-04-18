9525e693e49a564409e67efe4e5b7f0a40d3b7ea
Shanjida Rahman Maria <shanjidamaria07@gmail.com>
2022-04-18 20:52:30 +0600

    firebase hosting done

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -45,9 +45,6 @@ const Header = () => {
                 <Nav.Link as={Link} to="blogs">
                   <span className="link">BLOGS</span>
                 </Nav.Link>
-                <Nav.Link href="home#services">
-                  <span className="link">Services</span>
-                </Nav.Link>
                 <Nav.Link as={Link} to="aboutme">
                   <span className="link">ABOUT ME</span>
                 </Nav.Link>
